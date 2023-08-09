b77da629a39c25ee53e54730d83164b9f0e0603b
Patrick Nguyen <patar.nguyen@gmail.com>
2023-08-09 17:13:41 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Patrick Nguyen on 2023-08-09.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
